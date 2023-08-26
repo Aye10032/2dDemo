545b4507ca8f85996348912d005ecbb58ec82070
Aye10032 <aye10032@gmail.com>
2023-08-26 10:54:09 +0800

    跳跃动画状态机设置

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 10f;
 
+    private int _moveStatus;
+
     //翻转人物贴图
     private readonly Vector3 _flippedScale = new Vector3(-1, 1, 1);
     private float _moveX;
@@ -13,11 +15,15 @@ public class PlayerController : MonoBehaviour
 
     //刚体组件
     private Rigidbody2D _rigidbody2D;
+    //动画组件
+    private Animator _animator;
+    private static readonly int Movement1 = Animator.StringToHash("movement");
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,6 +41,22 @@ public class PlayerController : MonoBehaviour
 
         //改变人物速度
         _rigidbody2D.velocity = new Vector2(_moveX * moveSpeed, _rigidbody2D.velocity.y);
+
+        //改变动画状态
+        if (_moveX > 0)
+        {
+            _moveStatus = 1;
+        }
+        else if (_moveX < 0)
+        {
+            _moveStatus = -1;
+        }
+        else
+        {
+            _moveStatus = 0;
+        }
+        
+        _animator.SetInteger(Movement1, _moveStatus);
     }
 
     //改变人物朝向
