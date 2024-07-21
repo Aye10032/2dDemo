aaf93849d4379ada026fb650ec38ea0032abf0e9
Aye10032 <aye10032@gmail.com>
2024-07-21 10:55:19 +0800

    行走及待机动画

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private readonly Vector3 _flippedScale = new(-1, 1, 1);
-
     private float _moveX;
+    private float _moveY;
+    private int _moveChangeAnim;
+
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+    public float moveSpeed = 10f;
+    private static readonly int MovementId = Animator.StringToHash("movement");
 
     // Start is called before the first frame update
     void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,6 +32,19 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         _moveX = Input.GetAxis("Horizontal");
+        _moveY = Input.GetAxisRaw("Vertical");
+
+        _rigidbody2D.velocity = new Vector2(_moveX * moveSpeed, _rigidbody2D.velocity.y);
+
+        // 就是switch
+        _moveChangeAnim = _moveX switch
+        {
+            > 0 => 1,
+            < 0 => -1,
+            _ => 0
+        };
+
+        _animator.SetInteger(MovementId, _moveChangeAnim);
     }
 
     private void Direction()
